27059dcb581888719f24c2424ca64b5d2e346aa5
Hugh Brock <hbrock@gmail.com>
2014-05-09 15:48:45 -0400

    update user status popup

--- bbpress/auth-modal.php
+++ bbpress/auth-modal.php
@@ -61,12 +61,14 @@
 	      <?php bbp_user_profile_link(bbp_get_current_user_id()); ?>
 	      
 	      <ol class="links">
-		<li><a href="/membership-account/">Your Account</a> (change address, change password)</li>
+		<li><a href="/membership-account/">Your membership account</a> 
+                  (change private information: address, password)</li>
+		<li><a href="<?php bbp_user_profile_url(bbp_get_current_user_id()); ?>/profile">Your profile</a> 
+                  (change public information: your bio, your harp information)</li>
                 <?php if (pmpro_hasMembershipLevel(array(2,3,4,5,6))): ?>
 		  <li>This month's <a href="/category/magazine/">Harp Column</a></li>
                 <?php else: ?>
-		  <li>You are not a subscriber! 
-                    <a href="/membership-account/subscribe/">Subscribe now</a> to receive full access to Harp Column online.</a></li>
+		  <li><a href="/membership-account/subscribe/">Subscribe now</a> to receive full access to Harp Column online.</a></li>
                 <?php endif; ?>
 	      </ol>
 	      

c3ac669c4ac928ddec00a2cba3e5870cefe485dd
Hugh Brock <hbrock@gmail.com>
2014-12-05 16:18:13 -0500

    Add new page with events sidebar hard coded

--- events-sidebar-page.php
+++ events-sidebar-page.php
@@ -65,7 +65,7 @@ endif;
     <aside class="col-4 sidebar">
       <ul>
 	
-	<?php if (!dynamic_sidebar('primary-sidebar')) : ?>
+	<?php if (!get_sidebar('primary-sidebar')) : ?>
 	<?php _e("<li>Nothing yet.</li>", 'bunyad'); ?>
 	<?php endif; ?>
 	

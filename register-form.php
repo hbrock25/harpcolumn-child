ad4d6e00673dcfa0002940817df4864a446669d0
Hugh Brock <hbrock@gmail.com>
2015-10-19 08:32:37 +0200

    second register form fix

--- register-form.php
+++ register-form.php
@@ -5,7 +5,7 @@ Theme My Login will always look in your theme's directory first, before using th
 */
 ?>
 <div class="tml tml-register" id="theme-my-login<?php $template->the_instance(); ?>">
-  <h1>To register for Harp Column, please visit: <a href="/membership-account/subscribe/">The subscription page.</a></h1>
+  <h2>To register for Harp Column, please visit <a href="/membership-account/subscribe/">the subscription page.</a></h2>
 	<?php $template->the_action_links( array( 'register' => false ) ); ?>
 
 </div>

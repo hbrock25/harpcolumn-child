f85d9e457c02801112a4819d54cd9f63715a2530
Hugh Brock <hbrock@gmail.com>
2016-04-17 11:49:13 +0200

    Add back visual editor to bbpress

--- functions.php
+++ functions.php
@@ -209,18 +209,3 @@ function hc_add_back_register_link( $action_links, $args ) {
 
 add_filter( 'tml_action_links', 'hc_add_back_register_link', 10, 2);
 
-/*
-*function bbp_enable_visual_editor( $args = array() ) {
-*    $args['tinymce'] = true;
-*    $args['quicktags'] = false;
-*    $args['teeny'] = false;
-*    return $args;
-*}
-*add_filter( 'bbp_after_get_the_content_parse_args', 'bbp_enable_visual_editor' *);
-*
-*function bbp_tinymce_paste_plain_text( $plugins = array() ) {
-*    $plugins[] = 'paste';
-*    return $plugins;
-*}
-*add_filter( 'bbp_get_tiny_mce_plugins', 'bbp_tinymce_paste_plain_text' );
-*/

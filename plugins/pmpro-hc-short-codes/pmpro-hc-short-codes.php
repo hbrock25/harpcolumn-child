6b8e1c932adb9348cc57e98f34109ecd0b93466d
Hugh Brock <hbrock@gmail.com>
2016-11-05 19:04:37 +0100

    New plugin with HC short codes

--- plugins/pmpro-hc-short-codes/pmpro-hc-short-codes.php
+++ plugins/pmpro-hc-short-codes/pmpro-hc-short-codes.php
@@ -41,7 +41,7 @@ function pmpro_expiration_date_shortcode( $atts ) {
 
   //no user ID? bail
   if(!$user_id)
-		return '<strong><a href="/my-account">Login or Register</a></strong>';
+		return '<strong><a href="/my-account">Login</a> | <a href="/my-account">Register</a></strong>';
 
 	//get the user's level
 	$level = pmpro_getMembershipLevelForUser($user_id);

f7d1f08d3eee881ea8ac8688689b5b1766f2b7a4
Hugh Brock <hbrock@gmail.com>
2016-11-07 06:36:14 +0100

    Correct renewal link

--- plugins/pmpro-hc-short-codes/pmpro-hc-short-codes.php
+++ plugins/pmpro-hc-short-codes/pmpro-hc-short-codes.php
@@ -47,7 +47,7 @@ function pmpro_expiration_date_shortcode( $atts ) {
 	$level = pmpro_getMembershipLevelForUser($user_id);
 
 	if(!empty($level) && !empty($level->enddate) && $level->id > 1)
-		$content = 'Your subscription expires on ' . date(get_option('date_format'), $level->enddate) . '. <a href="/subscribe">Renew</a> | <a href="/my-account">My Account</a> | <a href="/my-account/customer-logout">Logout</a>';
+		$content = 'Your subscription expires on ' . date(get_option('date_format'), $level->enddate) . '. <a href="/renew">Renew</a> | <a href="/my-account">My Account</a> | <a href="/my-account/customer-logout">Logout</a>';
 	else
 		$content = '<a href="/subscribe">Subscribe</a> | <a href="/my-account">My Account</a> | <a href="/my-account/customer-logout">Logout</a>';
 

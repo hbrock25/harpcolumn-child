c875bb628174be1e88df4366d9963f5937af934e
Hugh Brock <hbrock@gmail.com>
2016-11-20 13:42:41 +0100

    hc-member-pages genesis

---
+++ plugins/hc-member-pages/hc-member-pages.php
@@ -0,0 +1,46 @@
+<?php
+/*
+   Plugin Name: Paid Memberships Pro: HC Member Admin Pages
+   Plugin URI: http://www.harpcolumn.com
+   Description: Member list and CSV exports for HC/PMPro
+   Version: 1.0
+   Requires: 4.5.3
+   Author: Hugh Brock <hbrock@harpcolumn.com>
+   Author URI: http://www.harpcolumn.com
+ */
+
+function hc_members_list_page_html() {
+    //make sure PMPro is active
+    if(!function_exists('pmpro_getMembershipLevelForUser')) {
+	return;
+    }
+    // check user capabilities
+    if (!current_user_can('manage_options') ||
+	! current_user_can('pmpro_memberslist')) {
+        return;
+    }
+
+    
+// page starts here
+?>
+    <div><h1>Harp Column Member List Page</h1></div>
+<?php
+}
+
+function hc_members_list_page()
+{
+    //make sure PMPro is active
+    if(!function_exists('pmpro_getMembershipLevelForUser'))
+	return;
+
+    add_submenu_page(
+        'pmpro-membershiplevels',
+        'Harp Column Members List',
+        'Harp Column Members List',
+        'pmpro_memberslist',
+        'hc-members-list',
+        'hc_members_list_page_html'
+    );
+}
+add_action('admin_menu', 'hc_members_list_page');
+

13185e9b32d54faa36d4efabe0e08c221c1985a5
Hugh Brock <hbrock@gmail.com>
2016-11-20 17:17:14 +0100

    Add better require checks in plugin header, move main body to includes dir

--- plugins/hc-member-pages/hc-member-pages.php
+++ plugins/hc-member-pages/hc-member-pages.php
@@ -9,67 +9,54 @@
    Author URI: http://www.harpcolumn.com
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+    die( 'Access denied.' );
+}
+
+define( 'HCML_NAME','HC Member List Plugin' );
 define( 'HC_ML_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'HC_MEMBER_PAGE_SLUG', "hc-members-list" );
+define( 'HCML_REQUIRED_PHP_VERSION', '5.3' ); // because of get_called_class()
+define( 'HCML_REQUIRED_WP_VERSION',  '3.1' ); // because of esc_textarea()
 
-function hc_members_list_page_html() {
-    //make sure PMPro is active
-    if(!function_exists('pmpro_getMembershipLevelForUser')) {
-	return;
-    }
-    // check user capabilities
-    if (!current_user_can('manage_options') ||
-	! current_user_can('pmpro_memberslist')) {
-        return;
-    }
 
+/**
+ * Checks if the system requirements are met
+ *
+ * @return bool True if system requirements are met, false if not
+ */
+
+function HCML_requirements_met() {
+    global $wp_version;
+    require_once( ABSPATH . '/wp-admin/includes/plugin.php' ); // to get is_plugin_active() early
+    if ( version_compare( PHP_VERSION, HCML_REQUIRED_PHP_VERSION, '<' ) ) {
+	return false;
+    }
+    if ( version_compare( $wp_version, HCML_REQUIRED_WP_VERSION, '<' ) ) {
+	return false;
+    }
+    
+    if ( ! is_plugin_active( 'paid-memberships-pro/paid-memberships-pro.php' ) ) {
+	return false;
+    }
+	
+    if ( ! is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
+	return false;
+    }
+    return true;
+}
+
+/*
+ * Check requirements and load main class
+ * The main program needs to be in a separate file that only gets loaded if the plugin requirements are met. Otherwise older PHP installations could crash when trying to parse it.
+ */
+
+if ( wpps_requirements_met() ) {
     //vars
     global $wpdb, $pmpro_currency_symbol, $woocommerce;
-    
-    // headers first
-    require_once(PMPRO_DIR . "/adminpages/admin_header.php");
-    
-    if(!empty($_REQUEST['user_id'])) {
-	// get the data from the model
-	$user_id = $_REQUEST['user_id'];
-	$pmbaddr = pretty_pmpro_billing_address( $user_id );
-	$pmsaddr = pretty_pmpro_shipping_address( $user_id );
-	$woobaddr = pretty_woo_billing_address( $user_id );
-	$woosaddr = pretty_woo_shipping_address( $user_id );
-	require( HC_ML_PLUGIN_PATH . '/views/one-user.php' );
-    } else {
-	require( HC_ML_PLUGIN_PATH . '/memberslist.php' );
-    }
-    require_once(PMPRO_DIR . "/adminpages/admin_footer.php");
+    require_once( __DIR__ . '/includes/user-addresses.php' );
+    require_once( __DIR__ . '/includes/hc-member-pages.php' );
+} else {
+    die("Plugin requirements not met");
 }
 
-function hc_members_list_page()
-{
-    //make sure PMPro is active
-    if(!function_exists('pmpro_getMembershipLevelForUser'))
-	return;
-
-    add_submenu_page(
-        'pmpro-membershiplevels',
-        'Harp Column Members List',
-        'Harp Column Members List',
-        'pmpro_memberslist',
-        HC_MEMBER_PAGE_SLUG,
-        'hc_members_list_page_html'
-    );
-}
-add_action('admin_menu', 'hc_members_list_page', 100);
-
-function hc_members_list_admin_bar_menu() {
-
-    global $wp_admin_bar;
-    if(current_user_can('pmpro_memberslist')) {
-	$wp_admin_bar->add_menu( array(
-	    'id' => 'hc-members-list',
-	    'parent' => 'paid-memberships-pro',
-	    'title' => __( 'Harp Column Members List', 'pmpro'),
-	    'href' => get_admin_url(NULL, '/admin.php?page=hc-members-list')));
-    }
-}
-    
-add_action('admin_bar_menu', 'hc_members_list_admin_bar_menu', 1001);
---
+++ plugins/hc-member-pages/includes/hc-member-pages.php
@@ -0,0 +1,59 @@
+<?php 
+
+/* Main controller -- pages and functions */
+
+function hc_members_list_page_html() {
+
+/* Define the actions for the main page */
+
+    // check user capabilities
+    if (!current_user_can('manage_options') ||
+	! current_user_can('pmpro_memberslist')) {
+	return;
+    }
+
+    // headers first
+    require_once(PMPRO_DIR . "/adminpages/admin_header.php");
+
+    if(!empty($_REQUEST['user_id'])) {
+	// get the data from the model
+	$user_id = $_REQUEST['user_id'];
+	$pmbaddr = pretty_pmpro_billing_address( $user_id );
+	$pmsaddr = pretty_pmpro_shipping_address( $user_id );
+	$woobaddr = pretty_woo_billing_address( $user_id );
+	$woosaddr = pretty_woo_shipping_address( $user_id );
+	require( HC_ML_PLUGIN_PATH . '/views/one-user.php' );
+    } else {
+	require( HC_ML_PLUGIN_PATH . '/memberslist.php' );
+    }
+    require_once(PMPRO_DIR . "/adminpages/admin_footer.php");
+}
+
+function hc_members_list_page() {
+
+    add_submenu_page(
+	'pmpro-membershiplevels',
+	'Harp Column Members List',
+	'Harp Column Members List',
+	'pmpro_memberslist',
+	HC_MEMBER_PAGE_SLUG,
+	'hc_members_list_page_html'
+    );
+}
+
+add_action('admin_menu', 'hc_members_list_page', 100);
+
+function hc_members_list_admin_bar_menu() {
+
+    global $wp_admin_bar;
+    if(current_user_can('pmpro_memberslist')) {
+	$wp_admin_bar->add_menu( array(
+	    'id' => 'hc-members-list',
+	    'parent' => 'paid-memberships-pro',
+	    'title' => __( 'Harp Column Members List', 'pmpro'),
+	    'href' => get_admin_url(NULL, '/admin.php?page=hc-members-list')));
+    }
+}
+
+add_action('admin_bar_menu', 'hc_members_list_admin_bar_menu', 1001);
+

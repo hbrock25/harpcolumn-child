9047e7c8231406a92cdde7fe0b12778560883ce4
Hugh Brock <hbrock@gmail.com>
2016-12-28 12:49:02 +0100

    Clean up memberslist a bit before adding the non-user get clause.

--- plugins/hc-member-pages/memberslist.php
+++ plugins/hc-member-pages/memberslist.php
@@ -3,58 +3,59 @@
 //only admins can get this
 if(!function_exists("current_user_can") || (!current_user_can("manage_options") && !current_user_can("pmpro_memberslist")))
 {
-die(__("You do not have permissions to perform this action.", "pmpro"));
+    die(__("You do not have permissions to perform this action.", "pmpro"));
 }  
 
 //vars
 if(isset($_REQUEST['s']))
-$s = $_REQUEST['s'];
+    $s = $_REQUEST['s'];
 else
-$s = "";
+    $s = "";
 
 if(isset($_REQUEST['l']))
-$l = $_REQUEST['l'];
+    $l = $_REQUEST['l'];
 else
-$l = false;
+    $l = false;
 
 ?>
 
-    <form id="posts-filter" method="get" action="">  
-	<h2>
-	    <?php _e('Members List', 'pmpro');?>
-	    <a target="_blank" href="<?php echo admin_url('admin-ajax.php');?>?action=hc_memberslist_csv&s=<?php echo $s?>&l=<?php echo $l?>" class="add-new-h2"><?php _e('Export to CSV', 'pmpro');?></a>
-	</h2>    
-	<ul class="subsubsub">
-	    <li>      
-		<?php _e('Show', 'pmpro');?>
-		<select name="l" onchange="jQuery('#posts-filter').submit();">
-		    <option value="" <?php if(!$l) { ?>selected="selected"<?php } ?>><?php _e('All Levels', 'pmpro');?></option>
-		    <option value="paid" <?php if($l == "paid") { ?>selected="selected"<?php } ?>><?php _e('Paid Subscribers', 'pmpro');?></option>
-		    <option value="paid_print_domestic" <?php if($l == "paid_print_domestic") { ?>selected="selected"<?php } ?>><?php _e('Paid Domestic Print Subscribers', 'pmpro');?></option>
-		    <option value="exp_last_60_print" <?php if($l == "exp_last_60_print") { ?>selected="selected"<?php } ?>><?php _e('Recently Expired Domestic Print Subs', 'pmpro');?></option>
-		    <option value="exp_next_month" <?php if($l == "exp_next_month") { ?>selected="selected"<?php } ?>><?php _e('Expires next month', 'pmpro');?></option>
-		    <option value="exp_next_2_3" <?php if($l == "exp_next_2_3") { ?>selected="selected"<?php } ?>><?php _e('Expires 2-3 months out', 'pmpro');?></option>
-		    <option value="exp_next_4_5" <?php if($l == "exp_next_4_5") { ?>selected="selected"<?php } ?>><?php _e('Expires 4-5 months out', 'pmpro');?></option>
+<form id="posts-filter" method="get" action="">  
+    <h2>
+	<?php _e('Members List', 'pmpro');?>
+	<a target="_blank" href="<?php echo admin_url('admin-ajax.php');?>?action=hc_memberslist_csv&s=<?php echo $s?>&l=<?php echo $l?>" class="add-new-h2"><?php _e('Export to CSV', 'pmpro');?></a>
+    </h2>    
+    <ul class="subsubsub">
+	<li>      
+	    <?php _e('Show', 'pmpro');?>
+	    <select name="l" onchange="jQuery('#posts-filter').submit();">
+		<option value="" <?php if(!$l) { ?>selected="selected"<?php } ?>><?php _e('All Levels', 'pmpro');?></option>
+		<option value="paid" <?php if($l == "paid") { ?>selected="selected"<?php } ?>><?php _e('Paid Subscribers', 'pmpro');?></option>
+		<option value="new_non_subs" <?php if($l == "new_non_subs") { ?>selected="selected"<?php } ?>><?php _e('New Non-Subscribers', 'pmpro');?></option>
+		<option value="paid_print_domestic" <?php if($l == "paid_print_domestic") { ?>selected="selected"<?php } ?>><?php _e('Paid Domestic Print Subscribers', 'pmpro');?></option>
+		<option value="exp_last_60_print" <?php if($l == "exp_last_60_print") { ?>selected="selected"<?php } ?>><?php _e('Recently Expired Domestic Print Subs', 'pmpro');?></option>
+		<option value="exp_next_month" <?php if($l == "exp_next_month") { ?>selected="selected"<?php } ?>><?php _e('Expires next month', 'pmpro');?></option>
+		<option value="exp_next_2_3" <?php if($l == "exp_next_2_3") { ?>selected="selected"<?php } ?>><?php _e('Expires 2-3 months out', 'pmpro');?></option>
+		<option value="exp_next_4_5" <?php if($l == "exp_next_4_5") { ?>selected="selected"<?php } ?>><?php _e('Expires 4-5 months out', 'pmpro');?></option>
 
-		    <?php
-		    $levels = $wpdb->get_results("SELECT id, name FROM $wpdb->pmpro_membership_levels ORDER BY name");
-		    foreach($levels as $level)
-		    {
-		    ?>
-			<option value="<?php echo $level->id?>" <?php if($l == $level->id) { ?>selected="selected"<?php } ?>><?php echo $level->name?></option>
-                    <?php
-                    }
-		    ?>
-		    <option value="oldmembers" <?php if($l == "oldmembers") { ?>selected="selected"<?php } ?>><?php _e('Old Members', 'pmpro');?></option>
-		</select>      
-	    </li>
-	</ul>
-	<p class="search-box">
-	    <label class="hidden" for="post-search-input"><?php _e('Search Members', 'pmpro');?>:</label>
-	    <input type="hidden" name="page" value="hc-members-list" />    
-	    <input id="post-search-input" type="text" value="<?php echo $s?>" name="s"/>
-	    <input class="button" type="submit" value="<?php _e('Search Members', 'pmpro');?>"/>
-	</p>
+		<?php
+		$levels = $wpdb->get_results("SELECT id, name FROM $wpdb->pmpro_membership_levels ORDER BY name");
+		foreach($levels as $level)
+		{
+		?>
+		    <option value="<?php echo $level->id?>" <?php if($l == $level->id) { ?>selected="selected"<?php } ?>><?php echo $level->name?></option>
+                <?php
+                }
+		?>
+		<option value="oldmembers" <?php if($l == "oldmembers") { ?>selected="selected"<?php } ?>><?php _e('Old Members', 'pmpro');?></option>
+	    </select>      
+	</li>
+    </ul>
+    <p class="search-box">
+	<label class="hidden" for="post-search-input"><?php _e('Search Members', 'pmpro');?>:</label>
+	<input type="hidden" name="page" value="hc-members-list" />    
+	<input id="post-search-input" type="text" value="<?php echo $s?>" name="s"/>
+	<input class="button" type="submit" value="<?php _e('Search Members', 'pmpro');?>"/>
+    </p>
 	<?php 
 	//some vars for the search
 	if(isset($_REQUEST['pn']))
@@ -68,116 +69,63 @@ $l = false;
 	    $limit = 15;
 	
 	$end = $pn * $limit;
-	$start = $end - $limit;        
+	$start = $end - $limit;
+
+	$search_clause = "";
+	if $s {
+	    $search_clause = "AND (u.user_login LIKE '%$s%' OR u.user_email LIKE '%$s%' OR um.meta_value LIKE '%$s%')";
+	}
         
-	if($s)
-	{
-	    $sqlQuery = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, UNIX_TIMESTAMP(u.user_registered) as joindate, mu.membership_id, mu.initial_payment, mu.billing_amount, mu.cycle_period, mu.cycle_number, mu.billing_limit, mu.trial_amount, mu.trial_limit, UNIX_TIMESTAMP(mu.startdate) as startdate, UNIX_TIMESTAMP(mu.enddate) as enddate, m.name as membership FROM $wpdb->users u LEFT JOIN $wpdb->usermeta um ON u.ID = um.user_id LEFT JOIN $wpdb->pmpro_memberships_users mu ON u.ID = mu.user_id";
+	$sqlQuery = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, UNIX_TIMESTAMP(u.user_registered) as joindate, mu.membership_id, mu.initial_payment, mu.billing_amount, mu.cycle_period, mu.cycle_number, mu.billing_limit, mu.trial_amount, mu.trial_limit, UNIX_TIMESTAMP(mu.startdate) as startdate, UNIX_TIMESTAMP(mu.enddate) as enddate, m.name as membership FROM $wpdb->users u LEFT JOIN $wpdb->usermeta um ON u.ID = um.user_id LEFT JOIN $wpdb->pmpro_memberships_users mu ON u.ID = mu.user_id";
 
-	    if($l == "exp_last_60_print") {
-		$sqlQuery .= " AND mu.status = 'expired' AND mu.membership_id IN(2, 6) LEFT JOIN wp_pmpro_memberships_users mu2 ON u.ID = mu2.user_id AND mu2.status = 'active' AND mu2.membership_id = 1 "; 
-	    } elseif($l == "oldmembers") {
-		$sqlQuery .= " LEFT JOIN $wpdb->pmpro_memberships_users mu2 ON u.ID = mu2.user_id AND mu2.status = 'active' ";
-	    }
-
-	    $sqlQuery .= " LEFT JOIN $wpdb->pmpro_membership_levels m ON mu.membership_id = m.id WHERE mu.membership_id > 0 AND (u.user_login LIKE '%$s%' OR u.user_email LIKE '%$s%' OR um.meta_value LIKE '%$s%') ";        
-	    
-	    if($l == "oldmembers")
-		$sqlQuery .= " AND mu.status = 'inactive' AND mu2.status IS NULL ";
-
-	    // This is horrific, I should make it better
-	    elseif($l == "paid")
-	    $sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 7)";
-	    elseif($l == "paid_print_domestic")
-	    $sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 4, 5, 7)";
-	    elseif($l == "exp_last_60_print")
-	    $sqlQuery .= " AND date(mu.enddate) < CURDATE() AND date(mu.enddate) > (DATE_SUB(CURDATE(), INTERVAL 2 MONTH))";
-
-	    elseif($l == "exp_next_month")
-            // This is for renewal notices -- only do them for domestic and foreign non-agency subscribers
-	    $sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) AND (LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 1 MONTH)) >= date(mu.enddate))";
-	    elseif($l == "exp_next_2_3")
-	    $sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) AND (mu.enddate >= STR_TO_DATE(((PERIOD_ADD(EXTRACT(YEAR_MONTH FROM CURDATE()),2)*100)+1), '%Y%m%d')) AND (mu.enddate <= LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 3 MONTH)))";
-	    elseif($l == "exp_next_4_5")
-	    $sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) AND (mu.enddate >= STR_TO_DATE(((PERIOD_ADD(EXTRACT(YEAR_MONTH FROM CURDATE()),4)*100)+1), '%Y%m%d')) AND (mu.enddate <= LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 5 MONTH)))";
-	    elseif($l)
-	    $sqlQuery .= " AND mu.status = 'active' AND mu.membership_id = '" . $l . "' ";          
-	    else
-		$sqlQuery .= " AND mu.status = 'active' ";      
-	    
-	    $sqlQuery .= " GROUP BY u.ID ";
-	    
-	    if($l == "oldmembers")
-		$sqlQuery .= "ORDER BY enddate DESC ";
-	    else
-		$sqlQuery .= "ORDER BY u.user_registered DESC ";
-	    
-	    $sqlQuery .= "LIMIT $start, $limit";
+	if($l == "exp_last_60_print") {
+	    $sqlQuery .= " AND mu.status = 'expired' AND mu.membership_id IN(2, 6) LEFT JOIN wp_pmpro_memberships_users mu2 ON u.ID = mu2.user_id AND mu2.status = 'active' AND mu2.membership_id = 1 "; 
+	} elseif($l == "oldmembers") {
+	    $sqlQuery .= " LEFT JOIN $wpdb->pmpro_memberships_users mu2 ON u.ID = mu2.user_id AND mu2.status = 'active' ";
 	}
+
+	$sqlQuery .= " LEFT JOIN $wpdb->pmpro_membership_levels m ON mu.membership_id = m.id WHERE mu.membership_id > 0";
+
+	// append the search restriction if any
+	$sqlQuery .= $search_clause;
+	    
+	// Add the restrictions for the various member classes
+	if($l == "oldmembers")
+	    $sqlQuery .= " AND mu.status = 'inactive' AND mu2.status IS NULL ";
+	elseif($l == "paid")
+	$sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 7)";
+	elseif($l == "paid_print_domestic")
+	$sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 4, 5, 7)";
+	elseif($l == "exp_last_60_print")
+	$sqlQuery .= " AND date(mu.enddate) < CURDATE() AND date(mu.enddate) > (DATE_SUB(CURDATE(), INTERVAL 2 MONTH))";
+	elseif($l == "exp_next_month")
+        // This is for renewal notices -- only do them for
+	// domestic and foreign non-agency subscribers
+	$sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) AND (LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 1 MONTH)) >= date(mu.enddate))";
+	elseif($l == "exp_next_2_3")
+	$sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) AND (mu.enddate >= STR_TO_DATE(((PERIOD_ADD(EXTRACT(YEAR_MONTH FROM CURDATE()),2)*100)+1), '%Y%m%d')) AND (mu.enddate <= LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 3 MONTH)))";
+	elseif($l == "exp_next_4_5")
+	$sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) AND (mu.enddate >= STR_TO_DATE(((PERIOD_ADD(EXTRACT(YEAR_MONTH FROM CURDATE()),4)*100)+1), '%Y%m%d')) AND (mu.enddate <= LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 5 MONTH)))";
+	elseif($l)
+	$sqlQuery .= " AND mu.status = 'active' AND mu.membership_id = '" . $l . "' ";          
 	else
-	{
-	    $sqlQuery = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, UNIX_TIMESTAMP(u.user_registered) as joindate, mu.membership_id, mu.initial_payment, mu.billing_amount, mu.cycle_period, mu.cycle_number, mu.billing_limit, mu.trial_amount, mu.trial_limit, UNIX_TIMESTAMP(mu.startdate) as startdate, UNIX_TIMESTAMP(mu.enddate) as enddate, m.name as membership FROM $wpdb->users u LEFT JOIN $wpdb->pmpro_memberships_users mu ON u.ID = mu.user_id ";
+	    $sqlQuery .= " AND mu.status = 'active' ";      
+	
+	$sqlQuery .= " GROUP BY u.ID ";
 	    
-	    if($l == "exp_last_60_print") {
-		$sqlQuery .= " AND mu.status = 'expired' AND mu.membership_id IN (2, 6) LEFT JOIN wp_pmpro_memberships_users mu2 ON u.ID = mu2.user_id AND mu2.status = 'active' AND mu2.membership_id = 1 "; 
-	    } elseif($l == "oldmembers") {
-		$sqlQuery .= " LEFT JOIN $wpdb->pmpro_memberships_users mu2 ON u.ID = mu2.user_id AND mu2.status = 'active' ";
-	    }
-	    
-	    $sqlQuery .= " LEFT JOIN $wpdb->pmpro_membership_levels m ON mu.membership_id = m.id WHERE mu.membership_id > 0  ";
-	    
-	    if($l == "oldmembers")
-		$sqlQuery .= " AND mu.status = 'inactive' AND mu2.status IS NULL ";
-	    // This is horrific, I should make it better
-	    elseif($l == "paid")
-	    $sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 7)";
-	    elseif($l == "paid_print_domestic")
-	    $sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 4, 5, 7)";
-	    elseif($l == "exp_last_60_print")
-	    $sqlQuery .= " AND date(mu.enddate) < CURDATE() AND date(mu.enddate) > (DATE_SUB(CURDATE(), INTERVAL 2 MONTH))";
-	    elseif($l == "exp_next_month")
-	    $sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) AND (LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 1 MONTH)) >= date(mu.enddate))";
-	    elseif($l == "exp_next_2_3")
-	    $sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) AND (date(mu.enddate) >= STR_TO_DATE(((PERIOD_ADD(EXTRACT(YEAR_MONTH FROM CURDATE()),2)*100)+1), '%Y%m%d')) AND (date(mu.enddate) <= LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 3 MONTH)))";
-	    elseif($l == "exp_next_4_5")
-	    $sqlQuery .= " AND mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) AND (date(mu.enddate) >= STR_TO_DATE(((PERIOD_ADD(EXTRACT(YEAR_MONTH FROM CURDATE()),4)*100)+1), '%Y%m%d')) AND (date(mu.enddate) <= LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 5 MONTH)))";
-	    elseif($l)
-	    $sqlQuery .= " AND mu.status = 'active' AND mu.membership_id = '" . $l . "' ";
-	    else
-		$sqlQuery .= " AND mu.status = 'active' ";
-	    $sqlQuery .= " GROUP BY u.ID ";
-            
-	    if($l == "oldmembers")
-		$sqlQuery .= "ORDER BY enddate DESC ";
-	    else
-		$sqlQuery .= "ORDER BY u.user_registered DESC ";
-	    
-	    $sqlQuery .= "LIMIT $start, $limit";
-	}
+	if($l == "oldmembers")
+	    $sqlQuery .= "ORDER BY enddate DESC ";
+	else
+	    $sqlQuery .= "ORDER BY u.user_registered DESC ";
+	
+	$sqlQuery .= "LIMIT $start, $limit";
 
 	$sqlQuery = apply_filters("pmpro_members_list_sql", $sqlQuery);
-        
+
+	// Query assembled, now get the results
 	$theusers = $wpdb->get_results($sqlQuery);
 	$totalrows = $wpdb->get_var("SELECT FOUND_ROWS() as found_rows");
 	
-	if($theusers)
-	{
-	    $calculate_revenue = apply_filters("pmpro_memberslist_calculate_revenue", false);
-	    if($calculate_revenue)
-	    {
-		$initial_payments = pmpro_calculateInitialPaymentRevenue($s, $l);
-		$recurring_payments = pmpro_calculateRecurringRevenue($s, $l);      
-	?>
-	    <p class="clear"><?php echo strval($totalrows)?> members found. These members have paid <strong>$<?php echo number_format($initial_payments)?> in initial payments</strong> and will generate an estimated <strong>$<?php echo number_format($recurring_payments)?> in revenue over the next year</strong>, or <strong>$<?php echo number_format($recurring_payments/12)?>/month</strong>. <span class="pmpro_lite">(This estimate does not take into account trial periods or billing limits.)</span></p>
-	<?php
-	}
-	else
-	{
-	?>
-	    <p class="clear"><?php printf(__("%d members found.", "pmpro"), $totalrows);?></span></p>      
-        <?php
-        }
-	}    
 	?>
 	<table class="widefat">
 	    <thead>
@@ -301,13 +249,13 @@ $l = false;
 		?>    
 	    </tbody>
 	</table>
-    </form>
-    
-    <?php
-    echo pmpro_getPaginationString($pn, $totalrows, $limit, 1, add_query_arg(array("s" => urlencode($s), "l" => $l, "limit" => $limit)));
+</form>
 
-    // The above should Just Work, if so remove the string below.
-    //    echo pmpro_getPaginationString($pn, $totalrows, $limit, 1, get_admin_url(NULL, "/admin.php?page=" . HC_MEMBER_PAGE_SLUG . "&s=" . urlencode($s)), "&l=$l&limit=$limit&pn=");
+<?php
+echo pmpro_getPaginationString($pn, $totalrows, $limit, 1, add_query_arg(array("s" => urlencode($s), "l" => $l, "limit" => $limit)));
+
+// The above should Just Work, if so remove the string below.
+//    echo pmpro_getPaginationString($pn, $totalrows, $limit, 1, get_admin_url(NULL, "/admin.php?page=" . HC_MEMBER_PAGE_SLUG . "&s=" . urlencode($s)), "&l=$l&limit=$limit&pn=");
 
 
 /*    require_once(dirname(__FILE__) . "/admin_footer.php"); */

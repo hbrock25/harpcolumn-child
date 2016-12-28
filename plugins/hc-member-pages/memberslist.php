35e70b1049fa1e723dda1f20bb8cad6f98d23782
Hugh Brock <hbrock@gmail.com>
2016-12-28 16:44:17 +0100

    Start to refactor memberslist, remove oldmembers view, fix one bug for Alison

--- plugins/hc-member-pages/memberslist.php
+++ plugins/hc-member-pages/memberslist.php
@@ -17,6 +17,143 @@ if(isset($_REQUEST['l']))
 else
     $l = false;
 
+//some vars for the search
+if(isset($_REQUEST['pn']))
+    $pn = $_REQUEST['pn'];
+else
+    $pn = 1;
+
+if(isset($_REQUEST['limit']))
+    $limit = $_REQUEST['limit'];
+else
+    $limit = 15;
+
+$end = $pn * $limit;
+$start = $end - $limit;
+
+// build the query
+
+// selects
+$select_clause = "
+SELECT 
+SQL_CALC_FOUND_ROWS 
+u.ID, 
+u.user_login, 
+u.user_email, 
+UNIX_TIMESTAMP(u.user_registered) as joindate, 
+mu.membership_id, 
+mu.initial_payment, 
+mu.billing_amount, 
+mu.cycle_period,
+mu.cycle_number,
+mu.billing_limit,
+mu.trial_amount,
+mu.trial_limit,
+UNIX_TIMESTAMP(mu.startdate) as startdate,
+UNIX_TIMESTAMP(mu.enddate) as enddate,
+m.name as membership 
+";
+
+// from/joins
+
+// condititional joins
+if($l == "exp_last_60_print") {
+    $cond_join = " 
+LEFT JOIN $wpdb->pmpro_memberships_users mu 
+  ON u.ID = mu.user_id 
+AND mu.status = 'expired' 
+AND mu.membership_id IN(2, 6) 
+LEFT JOIN $wpdb->pmpro_memberships_users mu2 
+  ON u.ID = mu2.user_id AND mu2.status = 'active' 
+  AND mu2.membership_id IN(0, 1) ";
+
+} else {
+    $cond_join = "
+LEFT JOIN $wpdb->pmpro_memberships_users mu 
+  ON u.ID = mu.user_id 
+";
+}
+
+// These are always in
+$from_clause = "
+FROM $wpdb->users u 
+LEFT JOIN $wpdb->usermeta um 
+  ON u.ID = um.user_id "
+	     . $cond_join; // one of the two above
+
+// WHERE stuff
+
+$search_clause = "";
+if($s) {
+    $search_clause = " AND (u.user_login LIKE '%" . esc_sql($s) . "%' "
+		   . "  OR u.user_email LIKE '%" . esc_sql($s) . "%' "
+		   . "  OR um.meta_value LIKE '%" . esc_sql($s) . "%')";
+}
+
+// Choose the restriction for the various member classes, etc.
+
+switch ($l) {
+    case "paid":
+	$restriction = " mu.status = 'active' "
+		     . "AND mu.membership_id NOT IN(0, 1, 7)";
+	break;
+    case "paid_print_domestic":
+	$restriction = " mu.status = 'active' "
+		     . "AND mu.membership_id NOT IN(0, 1, 3, 4, 5, 7)";
+	break;
+    case "exp_last_60_print":
+	$restriction = " date(mu.enddate) < CURDATE() "
+		     . "AND date(mu.enddate) > (DATE_SUB(CURDATE(), INTERVAL 2 MONTH))";
+	break;
+    case "exp_next_month"
+	// This is for renewal notices -- only do them for
+	// domestic and foreign non-agency subscribers
+	$restriction = " mu.status = 'active' "
+		     . "AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) "
+		     . " AND (LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 1 MONTH)) "
+		     . "  >= date(mu.enddate))";
+	break;
+    case "exp_next_2_3":
+	$restriction = " mu.status = 'active' "
+		     . "AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) "
+		     . "AND (mu.enddate >= "
+		     . "  STR_TO_DATE(((PERIOD_ADD(EXTRACT(YEAR_MONTH FROM CURDATE()),2)*100)+1), '%Y%m%d')) "
+		     . "AND (mu.enddate <= LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 3 MONTH)))";
+	break;
+    case "exp_next_4_5":
+	$restriction = " mu.status = 'active' "
+		     . "AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9)"
+		     . "AND (mu.enddate >= STR_TO_DATE(((PERIOD_ADD(EXTRACT(YEAR_MONTH FROM CURDATE()),4)*100)+1), '%Y%m%d')) "
+		     . "AND (mu.enddate <= LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 5 MONTH)))";
+	break;
+    case "new_non_subs":
+	// They have no membership, or a guest membership, and they joined
+	// less than 60 days ago
+	$restriction = " (mu.membership_id IN(0, 1) OR mu.user_id IS NULL) "
+		     . "AND u.user_registered >= DATE_SUB(CURDATE(), INTERVAL 2 MONTH)";
+	break;
+    case true:
+	$restriction = " mu.status = 'active' "
+		     . "AND mu.membership_id = '" . esc_sql($l) . "' ";
+	break;
+    default:
+	$restriction = " mu.status = 'active' ";
+}
+
+$where_clause = "WHERE " . $restriction . $search_clause;
+
+
+$sqlQuery = $select_clause . $from_clause . $where_clause
+	  . " GROUP BY u.ID "
+	  . " ORDER BY u.user_registered DESC ";
+
+if($limit)
+    $sqlQuery .= "LIMIT $start, $limit";
+
+// Query assembled, now get the results
+$theusers = $wpdb->get_results($sqlQuery);
+$totalrows = $wpdb->get_var("SELECT FOUND_ROWS() as found_rows");
+
 ?>
 
 <form id="posts-filter" method="get" action="">  
@@ -46,7 +183,6 @@ else
                 <?php
                 }
 		?>
-		<option value="oldmembers" <?php if($l == "oldmembers") { ?>selected="selected"<?php } ?>><?php _e('Old Members', 'pmpro');?></option>
 	    </select>      
 	</li>
     </ul>
@@ -56,83 +192,6 @@ else
 	<input id="post-search-input" type="text" value="<?php echo $s?>" name="s"/>
 	<input class="button" type="submit" value="<?php _e('Search Members', 'pmpro');?>"/>
     </p>
-	<?php 
-	//some vars for the search
-	if(isset($_REQUEST['pn']))
-	    $pn = $_REQUEST['pn'];
-	else
-	    $pn = 1;
-	
-	if(isset($_REQUEST['limit']))
-	    $limit = $_REQUEST['limit'];
-	else
-	    $limit = 15;
-	
-	$end = $pn * $limit;
-	$start = $end - $limit;
-
-	$search_clause = "";
-	if($s) {
-	    $search_clause = "AND (u.user_login LIKE '%" . esc_sql($s) . "%' OR u.user_email LIKE '%" . esc_sql($s) . "%' OR um.meta_value LIKE '%" . esc_sql($s) . "%')";
-	}
-
-	$sqlQuery = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, UNIX_TIMESTAMP(u.user_registered) as joindate, mu.membership_id, mu.initial_payment, mu.billing_amount, mu.cycle_period, mu.cycle_number, mu.billing_limit, mu.trial_amount, mu.trial_limit, UNIX_TIMESTAMP(mu.startdate) as startdate, UNIX_TIMESTAMP(mu.enddate) as enddate, m.name as membership FROM $wpdb->users u LEFT JOIN $wpdb->usermeta um ON u.ID = um.user_id LEFT JOIN $wpdb->pmpro_memberships_users mu ON u.ID = mu.user_id";
-
-	if($l == "exp_last_60_print") {
-	    $sqlQuery .= " AND mu.status = 'expired' AND mu.membership_id IN(2, 6) LEFT JOIN wp_pmpro_memberships_users mu2 ON u.ID = mu2.user_id AND mu2.status = 'active' AND mu2.membership_id = 1 "; 
-	} elseif($l == "oldmembers") {
-	    $sqlQuery .= " LEFT JOIN $wpdb->pmpro_memberships_users mu2 ON u.ID = mu2.user_id AND mu2.status = 'active' ";
-	}
-
-	$sqlQuery .= " LEFT JOIN $wpdb->pmpro_membership_levels m ON mu.membership_id = m.id WHERE ";
-
-	// where clause used to contain mu.membership_id > 0 
-	
-	// Add the restrictions for the various member classes
-	if($l == "oldmembers")
-	    $sqlQuery .= " mu.status = 'inactive' AND mu2.status IS NULL ";
-	elseif($l == "paid")
-	$sqlQuery .= " mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 7)";
-	elseif($l == "paid_print_domestic")
-	$sqlQuery .= " mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 4, 5, 7)";
-	elseif($l == "exp_last_60_print")
-	$sqlQuery .= " date(mu.enddate) < CURDATE() AND date(mu.enddate) > (DATE_SUB(CURDATE(), INTERVAL 2 MONTH))";
-	elseif($l == "exp_next_month")
-        // This is for renewal notices -- only do them for
-	// domestic and foreign non-agency subscribers
-	$sqlQuery .= " mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) AND (LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 1 MONTH)) >= date(mu.enddate))";
-	elseif($l == "exp_next_2_3")
-	$sqlQuery .= " mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) AND (mu.enddate >= STR_TO_DATE(((PERIOD_ADD(EXTRACT(YEAR_MONTH FROM CURDATE()),2)*100)+1), '%Y%m%d')) AND (mu.enddate <= LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 3 MONTH)))";
-	elseif($l == "exp_next_4_5")
-	$sqlQuery .= " mu.status = 'active' AND mu.membership_id NOT IN(0, 1, 3, 7, 8, 9) AND (mu.enddate >= STR_TO_DATE(((PERIOD_ADD(EXTRACT(YEAR_MONTH FROM CURDATE()),4)*100)+1), '%Y%m%d')) AND (mu.enddate <= LAST_DAY(DATE_ADD(CURDATE(), INTERVAL 5 MONTH)))";
-	elseif($l == "new_non_subs")
-	// They have no membership, or a guest membership, and they joined
-	// less than 60 days ago
-	$sqlQuery .= " (mu.user_id IS NULL OR mu.membership_id = 0) AND u.user_registered >= DATE_SUB(CURDATE(), INTERVAL 2 MONTH)";
-	elseif($l)
-	$sqlQuery .= " mu.status = 'active' AND mu.membership_id = '" . $l . "' ";          
-	else
-	    $sqlQuery .= " mu.status = 'active' ";      
-	
-	// append the search restriction if any
-	$sqlQuery .= $search_clause;
-	
-	$sqlQuery .= " GROUP BY u.ID ";
-	
-	if($l == "oldmembers")
-	    $sqlQuery .= "ORDER BY enddate DESC ";
-	else
-	    $sqlQuery .= "ORDER BY u.user_registered DESC ";
-	
-	$sqlQuery .= "LIMIT $start, $limit";
-
-	$sqlQuery = apply_filters("pmpro_members_list_sql", $sqlQuery);
-
-	// Query assembled, now get the results
-	$theusers = $wpdb->get_results($sqlQuery);
-	$totalrows = $wpdb->get_var("SELECT FOUND_ROWS() as found_rows");
-	
-	?>
     <p class="clear"><?php printf(__("%d members found.", "pmpro"), $totalrows);?></span></p>
     <table class="widefat">
 	<thead>
@@ -148,14 +207,7 @@ else
 		<th><?php _e('Membership', 'pmpro');?></th>  
 		<th><?php _e('Fee', 'pmpro');?></th>
 		<th><?php _e('Joined', 'pmpro');?></th>
-		<th>
-		    <?php 
-		    if($l == "oldmembers")
-			_e('Ended', 'pmpro');
-		    else
-			_e('Expires', 'pmpro');
-		    ?>
-		</th>
+		<th><?php _e('Expires', 'pmpro');?></th>
 		<th><?php _e('Orders', 'pmpro');?></th>		
 	    </tr>
 	</thead>
@@ -260,10 +312,4 @@ else
 
 <?php
 echo pmpro_getPaginationString($pn, $totalrows, $limit, 1, add_query_arg(array("s" => urlencode($s), "l" => $l, "limit" => $limit)));
-
-// The above should Just Work, if so remove the string below.
-//    echo pmpro_getPaginationString($pn, $totalrows, $limit, 1, get_admin_url(NULL, "/admin.php?page=" . HC_MEMBER_PAGE_SLUG . "&s=" . urlencode($s)), "&l=$l&limit=$limit&pn=");
-
-
-/*    require_once(dirname(__FILE__) . "/admin_footer.php"); */
 

394f0bf128cbcd679d4fb7b920d41854877922b2
Hugh Brock <hbrock@gmail.com>
2017-01-08 17:34:54 +0100

    Switch over to profile builder for extra registration fields

--- functions.php
+++ functions.php
@@ -203,7 +203,7 @@ function woo_hc_validate_extra_register_fields( $username, $email, $validation_e
     }
 }
 
-add_action( 'woocommerce_register_post', 'woo_hc_validate_extra_register_fields', 10, 3 );
+// add_action( 'woocommerce_register_post', 'woo_hc_validate_extra_register_fields', 10, 3 );
 
 
 /**
@@ -281,93 +281,8 @@ function wc_csv_export_modify_column_headers( $column_headers ) {
     
     return array_merge( $column_headers, $new_headers );
 }
-add_filter( 'wc_customer_order_csv_export_order_headers', 'wc_csv_export_modify_column_headers' );
+// add_filter( 'wc_customer_order_csv_export_order_headers', 'wc_csv_export_modify_column_headers' );
 
-// set the data for each for custom columns
-function wc_csv_export_modify_row_data( $order_data, $order, $csv_generator ) {
-    
-    $custom_data = array(
-	'user_login' => $order->get_user() ? $order->get_user()->user_login : "No Login"
-    );
-    
-    $new_order_data   = array();
-    $one_row_per_item = 'item' === $csv_generator->format_definition['row_type'];
-    if ( $one_row_per_item ) {
-
-	foreach ( $order_data as $data ) {
-	    $new_order_data[] = array_merge( (array) $data, $custom_data );
-	}
-
-    } else {
-
-	$new_order_data = array_merge( $order_data, $custom_data );
-    }
-
-    return $new_order_data;
-}
-add_filter( 'wc_customer_order_csv_export_order_row', 'wc_csv_export_modify_row_data', 10, 3 );
-
-
-/**
- * CSV Customer Export Column Headers.
- *
- * Filter the column headers for the customer export
- *
- * @param array $column_headers {
- *     column headers in key => name format
- * }
- * @return array column headers in column_key => column_name format
- */
-function sv_wc_customer_csv_export_column_headers( $column_headers ) {
-
-    $new_headers = array();
-
-    foreach( $column_headers as $key => $value ) {
-	$new_headers[ $key ] = $value;
-
-	// Adds new columns after 'customer_id' column
-	if ( 'customer_id' === $key ) {
-	    // Add columns for each piece of data
-	    $new_headers['username'] = 'username';
-	}
-	
-	// Adds new column after 'last_name' column
-	if ( 'last_name' === $key ) {
-	    $new_headers['role'] = 'role';
-	}
-    }
-    return $new_headers;
-}
-add_filter( 'wc_customer_order_csv_export_customer_headers', 'sv_wc_customer_csv_export_column_headers' );
-
-
-/**
- * CSV Customer Export Row.
- *
- * Filter the individual row data for the customer export
- *
- * @param array $customer_data {
- *     order data in key => value format
- *     to modify the row data, ensure the key matches any of the header keys and set your own value
- * }
- * @param \WP_User $user WP User object
- * @return array customer data in the format key => content
- */
-function sv_wc_customer_csv_export_customer_row( $customer_data, $user ) {
-
-    $username = '';
-    $role = '';
-
-    // Only add data for non-guests
-    if ( 0 !== $user->ID ) {
-	$username = $user->user_login;
-	
-	$_roles = $user->roles;
-	$role = implode( ",", $_roles );
-    }
-    return array_merge( array( 'role' => $role, 'username' => $username ), $customer_data );
-}
-add_filter( 'wc_customer_order_csv_export_customer_row', 'sv_wc_customer_csv_export_customer_row', 10, 2 );
 
 // remove the prime-mentions-results action since it makes the site really slow
 function hc_bp_friends_remove_action() {
